4dce3a6dc9d12d2d7e884acc82390041dd638d49
Cody Stepp <codystepp691@gmail.com>
2024-03-16 15:26:15 -0500

    added a childredn  to createBrowserRouter a path to make it load the home page on load

--- react-portfolio/src/components/Header.jsx
+++ react-portfolio/src/components/Header.jsx
@@ -5,7 +5,7 @@ function Header(){
     return(
         <header className="headerContainer">
             <div className="navbar">
-            <div className="logo"><Link to= "/"><a href = "#" >Codys Portfolio</a></Link></div>
+            <div className="logo"><Link to= "/"><a href = "#" >Cody`s Portfolio</a></Link></div>
                 <ul className = "links">
                     <li><Link to= "/home"><a href = "#" >Home</a></Link></li>
                     <li><Link to= "/projects"><a href = "#" >Projects</a></Link></li>
--- react-portfolio/src/main.jsx
+++ react-portfolio/src/main.jsx
@@ -1,21 +1,12 @@
 import ReactDOM from 'react-dom/client'
 import { createBrowserRouter, RouterProvider } from 'react-router-dom';
-// import React from 'react'
-// import App from './App.jsx'
+
 import './index.css'
 import App from './App.jsx';
 import Home from './pages/Home.jsx';
 import Projects from './pages/projects.jsx'
 import Resume from './pages/Resume.jsx';
 import Contactme from './pages/Contactme.jsx';
-
-
-
-// ReactDOM.createRoot(document.getElementById('root')).render(
-//   <React.StrictMode>
-//     <App />
-//   </React.StrictMode>,
-// )
 
 const router = createBrowserRouter ([
   {
@@ -37,6 +28,10 @@ const router = createBrowserRouter ([
       {
         path: '/contactme',
         element:<Contactme/>
+      },
+      { 
+        path: '/',
+        element: <Home /> 
       }
       
     ],
