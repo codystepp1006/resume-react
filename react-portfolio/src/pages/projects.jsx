2e3bc63bcc6cca7edcd340d5c770b51e3e0cd80c
Cody Stepp <codystepp691@gmail.com>
2024-03-16 14:19:56 -0500

    added images to projects with css hover effect, added links to each image that will take you to the corresponding repo in my github

--- react-portfolio/src/pages/projects.jsx
+++ react-portfolio/src/pages/projects.jsx
@@ -3,7 +3,25 @@ function Projects() {
 
     return(
         <div>
-            <h1>Projects</h1>
+
+             <div className = "repoImages">
+                    <img id = "image" src = "https://github.com/codystepp1006/note-taker-express.js/raw/main/other/Screenshot%202024-02-19%20171558.png"/>
+                    <div className="image__overlay">
+                    <div className="image__title"><a  target="_blank" href ="https://github.com/codystepp1006/note-taker-express.js"><p className="image__title">Note Taker using Express.js</p></a></div>
+                </div>
+             </div>
+             <div className = "repoImages">
+                    <a  target="_blank" href ="https://github.com/codystepp1006/Backend-Ecommerce-Sequelize"><img id = "image" src = "https://github.com/codystepp1006/SQL-Employee-Tracker/raw/main/Screenshot%202024-03-01%20143858.png"/></a>
+                    <div className="image__overlay">
+                    <div className="image__title"><a  target="_blank" href ="https://github.com/codystepp1006/SQL-Employee-Tracker"><p className="image__title">MySQL Employee Tracker</p></a></div>
+                </div>
+             </div>
+             <div className = "repoImages">
+                    <a  target="_blank" href ="https://github.com/codystepp1006/Backend-Ecommerce-Sequelize"><img id = "image" src = "https://github.com/codystepp1006/weather-dashboard-api/raw/main/Screenshot%202023-11-17%20185226.png"/></a>
+                    <div className="image__overlay">
+                    <div className="image__title"><a  target="_blank" href ="https://github.com/codystepp1006/weather-dashboard-api"><p className="image__title">Weather Dashboard API</p></a></div>
+                </div>
+             </div>
         </div>
     );
 }
