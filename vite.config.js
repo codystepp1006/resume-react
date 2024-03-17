1f670839beccf45011f6aebd9b177833e18fd5d2
Cody Stepp <codystepp691@gmail.com>
2024-03-16 20:12:43 -0500

    changed dev port

--- vite.config.js
+++ vite.config.js
@@ -8,5 +8,12 @@ export default defineConfig({
     rollupOptions: {
       input: 'src/App.jsx' // Update the entry point to your main JavaScript file
     }
-  }
+  },
+  define: {
+    __APP_VERSION__: JSON.stringify('v1.0.0'),
+    __API_URL__: 'window.__backend_api_url',
+  },
+  server: {
+    port: 3009,
+  },
 });
