b9f3b53e6d243d7d23db5c38513a524f6da6ba58
Cody Stepp <codystepp691@gmail.com>
2024-03-16 23:22:15 -0500

    moving to github pages

--- vite.config.js
+++ vite.config.js
@@ -9,11 +9,5 @@ export default defineConfig({
       input: 'src/App.jsx' // Update the entry point to your main JavaScript file
     }
   },
-  define: {
-    __APP_VERSION__: JSON.stringify('v1.0.0'),
-    __API_URL__: 'window.__backend_api_url',
-  },
-  server: {
-    port: 3009,
-  },
+  base: "/vite/"
 });
